824aa267ee968f1250e6e235ec3f84f4615e74ff
Lucien Lin <luciensday@gmail.com>
2023-11-24 17:01:56 +0000

    chatbot works. add Events clientReady

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,32 +1,33 @@
-const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 
+// Create a new client instance
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.DirectMessages,
-    // Intents.FLAGS.GUILDS,
-    // Intents.FLAGS.GUILD_MESSAGES,
-    // Add more intents as needed, Empty array here after trial
+    GatewayIntentBits.MessageContent,
   ],
 });
 
-client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Message event listener
-client.on("messageCreate", async (message) => {
-  if (!message.author.bot) {
-    try {
-      const dmChannel = await message.author.createDM();
-      dmChannel.send(`Echo ${message.content}`);
-    } catch (error) {
-      console.error("Error sending DM:", error);
-    }
+client.on(Events.MessageCreate, async (message) => {
+  // Ignore messages from bots to prevent infinite loops
+  if (message.author.bot) return;
+
+  // Respond to "ping" command
+  if (message.content.toLowerCase() === "ping") {
+    message.reply("Pong!");
   }
 });
 
+// Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
